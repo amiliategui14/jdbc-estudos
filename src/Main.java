18223d2ff87aa9911fde24ac7f539efbcdca3897
Rafaela Amiliategui <amiliateguirafaela@gmail.com>
2026-06-29 22:07:25 -0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection conexao = ConexaoDB.conectar()) {
+            ProdutoDAO produtoDAO = new ProdutoDAO(conexao);
+
+            //Lista todos os produtos
+            mostrarProdutos(produtoDAO);
+
+            //exemplo
+            Produto novoProduto1 = new Produto("Notebook", 10, 1999.99, "Em estoque");
+            Produto novoProduto2 = new Produto("Smartphone", 20, 2499.99, "Estoque Baixo");
+            Produto novoProduto3 = new Produto("Tablet", 15, 799.00, "Estoque Baixo");
+
+            produtoDAO.inserir(novoProduto1);
+            produtoDAO.inserir(novoProduto2);
+            produtoDAO.inserir(novoProduto3);
+
+            // Lista todos os produtos após a inserção
+            mostrarProdutos(produtoDAO);
+
+            //Consulta po ID
+            Produto produtoConsultado = produtoDAO.consultarPorId(3);
+            if (produtoConsultado != null) {
+                System.out.println("Produto encontrado: " + produtoConsultado.getNome());
+            } else {
+                System.out.println("Produto não encontrado");
+            }            
+        } catch (Exception e) {
+            System.err.println("Erro geral: " + e.getMessage());
+        }
+    }
+
+    // Listar produtos
+    private static void mostrarProdutos(ProdutoDAO produtoDAO) {
+        List<Produto> todosProdutos = produtoDAO.listarTodos();
+        if (todosProdutos.isEmpty()) {
+            System.out.println("Nenhum produto encontrado.");
+        } else {
+            System.out.println("Lista de produtos:");
+            for (Produto p : todosProdutos) {
+                System.out.println(p.getId() + ": " + p.getNome() + " - " + p.getPreco());
+            }
+        }
+    }
+}
